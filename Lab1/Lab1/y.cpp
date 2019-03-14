115019404d9e48e0266c56d9f4e6e07b8b971a53
CGYR <1876634198@qq.com>
2019-03-14 23:04:42 +0800

    完成文件读写与框架搭建

---
+++ Lab1/Lab1/y.cpp
@@ -0,0 +1,123 @@
+#include"yh.h"
+#pragma warning(disable:4996)
+/*
+WordNode* wordList[26];
+WordNode* maxList = NULL;
+int maxLen;
+WordNode* nowList = NULL;
+int nowLen;
+int nNum;//-n对应的找到的单词的数目
+int nSet;//-n对应的n值
+
+void writeResult(int mode);
+void writeError(int errorCode);
+bool gen(string path);
+*/
+
+bool gen(string path) {
+	for (int i = 0; i < 26; i++) {
+		wordList[i] = new WordNode();
+	}
+	std::ifstream in(path);
+	if (!in) {
+		writeError(1);
+		return false;
+	}
+	istreambuf_iterator<char> begin(in);
+	istreambuf_iterator<char> end;
+	string wordIn(begin, end);
+
+	string wordTmp = "";
+	transform(wordIn.begin(), wordIn.end(), wordIn.begin(), ::tolower);
+	char charTmp = wordIn[0];
+	int i = 0;
+	int rank = 0;
+	int len = 0;
+	while (charTmp!='\0') {
+		if (charTmp < 97 || charTmp > 122) {
+			if (wordTmp != "" && len >1) {
+				WordNode *Node = new WordNode(wordTmp);
+				rank = wordTmp[0] - 97;
+				Node->next = wordList[rank];
+				wordList[rank] = Node;
+				wordTmp = "";
+				len = 0;
+			}
+			else{}
+		}
+		else {
+			wordTmp += charTmp;
+			len++;
+		}
+		charTmp = wordIn[++i];
+	}
+	return true;
+}
+
+// 将错误代码写入solution.txt
+// 在命令行运行模式中，则直接print一个错误后推出
+// 输入：错误编号
+void writeError(int errorCode) {
+	ofstream outfile;
+	outfile.open("solution.txt");
+	// 根据错误编号，将错误原因打出/写入/退出
+	// 需要更多的错误，可添加
+	switch (errorCode) {
+	case 1: {
+		printf("文件不存在！！！！！\n");
+		outfile << "-1" << endl;
+		exit(0);
+	}
+	case 2: {
+		printf("错误2\n");
+		outfile << "-2" << endl;
+		exit(0);
+	}
+	}
+	outfile.close();
+}
+
+// 模式1: 针对-w -c 只需要输出一个单词串的情况
+// 模式2: 针对-n 需要输出多个单词串的情况
+void writeResult(int mode) {
+	string result = "";
+	if (mode == 1) {
+		WordNode* nextNode = maxList;
+		while (nextNode != NULL) {
+			result = result + nextNode->word + "\n";
+			nextNode = nextNode->next;
+		}
+	}
+	else if (mode == 2) {
+		WordNode* nextNode = maxList;
+		int count = 1;
+		result += to_string(nNum) + "\n";
+		while (nextNode != NULL) {
+			if (count != 1 && count%nSet == 0) {
+				result = result + nextNode->word + "\n\n";
+			}
+			else {
+				result = result + nextNode->word + "\n";
+			}
+			count++;
+			nextNode = nextNode->next;
+		}
+	}
+	else {
+		// 报错
+	}
+	ofstream outfile;
+	outfile.open("solution.txt");
+	outfile << result << endl;
+	outfile.close();
+}
+
+
+
+int main(int argc, char* argv[]) {
+	gen("F://google/test.txt");
+	while (true)
+	{
+
+	}
+}
--- Lab1/Lab1/yh.h
+++ Lab1/Lab1/yh.h
@@ -1,6 +1,8 @@
 #pragma once
 #include<stdio.h>
+#include <algorithm>
 #include<iostream>
+#include<fstream>
 #include<stdlib.h>
 #include<vector>
 #include<string>
@@ -14,7 +16,7 @@ public:
 	int len;
 	bool uFlag = false;
 	WordNode* next = NULL;
-
+	WordNode::WordNode();
 	WordNode(string inw);
 	~WordNode();
 
@@ -25,11 +27,28 @@ private:
 WordNode::WordNode(string inw)
 {
 	word = inw;
-	len = word.length;
-	tail = word[word.length-1] - 97;
+	len = word.length();
+	tail = word[word.length()-1] - 97;
 }
+
+WordNode::WordNode()
+{
+	
+}
+
 
 WordNode::~WordNode()
 {
 }
 
+WordNode* wordList[26];
+WordNode* maxList = NULL;
+int maxLen;
+WordNode* nowList = NULL;
+int nowLen;
+int nNum;//-n对应的找到的单词的数目
+int nSet;//-n对应的n值
+
+void writeResult(int mode);
+void writeError(int errorCode);
+bool gen(string path);
